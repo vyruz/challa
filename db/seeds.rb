92137d2edae967de1d63b5ecca8895bdb06f6820
kburgess <kenneth.burgess@viasat.com>
2015-07-19 10:49:48 -0700

    added individual seed code

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 users = User.create!([{ username: 'Tester', email: 'tester@testing.com', points: 0}])
+sam = User.create!(username: 'scameron', email: 'samantha.cameron@viasat.com', password_digest: 'asdfghjkl;', role: 'challenger', points:200)
+x = Challenge.create!(name: 'Climb a flag pole')
+bounty = Bounty.create!(challenge_id: x.id, user_id: sam.id, points: 50)
 catagories = Category.create!([{name: 'Education'}, {name: 'Humanitarian'}])
 challenges = Challenge.create!([{name: 'test challenge 1', description: " it's just a test", category_id: catagories.first}])
