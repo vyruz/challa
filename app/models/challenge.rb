186bf3f7863d04a425811d1d6b992495b00f1384
Matthew Leibold <leiboldm@umich.edu>
2015-07-19 12:46:52 -0700

    i dont know

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,4 +1,6 @@
 class Challenge < ActiveRecord::Base
+  attr_accessor :image_path
+  attr_accessor :bounty_total
   belongs_to :category
   has_many :user_challenges
   has_many :users, through: :user_challenges
